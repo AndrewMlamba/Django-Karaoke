4beca9c354078dfeca3e53c14c160be325913d40
AndrewMlamba <andrew.mlamba@outlook.com>
2017-03-15 12:41:32 +0300

    Added code for the models in songs app

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Write your models here
+
+class Performer(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name  # returns the model's name attribute when turned into a string
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    length = models.IntegerField()
+    performer = models.ForeignKey(Performer)
+
+    def __str__(self):
+        # return "%s by %s" % (self.title, self.artist)
+        return "{} by {}".format(self.title, self.artist)
