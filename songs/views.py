85d501aa867d3e382d4d1ab837cf693e898867eb
AndrewMlamba <andrew.mlamba@outlook.com>
2017-03-15 13:31:55 +0300

    Add code for list view, all of the songs

--- songs/views.py
+++ songs/views.py
@@ -1,1 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+
+from django.http import Http404
+
+from .models import Song, Performer
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'songs/song_list.html', {'songs': songs})
+
+def
